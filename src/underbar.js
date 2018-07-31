c486822a6094ac4fb35dbd2b2085a27e9f41bc6c
Meifang Lin <meiflin123@gmail.com>
2018-07-31 13:14:54 -0700

    implement function sortBy

--- src/underbar.js
+++ src/underbar.js
@@ -401,6 +401,21 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+     if (!collection.length) {
+      throw new TypeError('Collection must be an array.');
+    }
+
+    if (Object.prototype.toString.call(iterator) === '[object String]') {
+      var iter = iterator;
+      iterator = function(item) {
+        return item[iter];
+      };
+    }
+
+    return collection.sort(function(a, b) {
+      return iterator(a) - iterator(b);
+    });
+      
   };
 
   // Zip together two or more arrays with elements of the same index
